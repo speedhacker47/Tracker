ee2a12f22c04b9e1ca4293134844918e7daf11d7
Chhatrapal Sahu <lazy.coder2024@gmail.com>
2026-03-13 01:13:17 +0530

    cors + remove rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -5,37 +5,6 @@ const nextConfig = {
   images: {
     remotePatterns: [],
   },
-
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://13.205.187.228/api/:path*',
-      },
-    ];
-  },
-
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
-          },
-        ],
-      },
-    ];
-  },
 };
 
-export default nextConfig;
+export default nextConfig;
